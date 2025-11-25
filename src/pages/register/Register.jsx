4cbc2706bbe5c3fc30aa555054faaf698a4070ce
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-11-25 21:12:48 +0600

    work on goin to the register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,29 +2,31 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className=" flex-col lg:flex-row-reverse">
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
-                <input
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="flex justify-center items-center mx-auto h-screen">
+      <form className="border w-[570px] h-[591px] p-12">
+        <h1 className="text-[24px] font-bold">Create an account</h1>
+        <fieldset className="fieldset">
+          {/* <label className="label">First Name</label> */}
+          <input
+            className="border-b-2 py-3"
+            type="text"
+            name="name"
+            placeholder="First Name"
+          />
+          {/* <label className="label">Last Name</label>
+          <input type="text" className="border-b-2 " />
+
+          <label className="label">Username or Email</label>
+          <input type="email" className="border-b-2 " />
+          <label className="label">Password</label>
+          <input type="password" className="border-b-2 " />
+          <label className="label">Confirm Password</label>
+          <input type="password" className="border-b-2" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div> */}
+        </fieldset>
+      </form>
     </div>
   );
 };
