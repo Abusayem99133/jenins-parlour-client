bab821d17536405c16a5e4aa13887832c2c5a7aa
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-11-25 22:43:17 +0600

    register page ui work done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import lineBar from "../../assets/image/register/Line -bar.png";
+import facebook from "../../assets/image/register/facebook.png";
+import { FcGoogle } from "react-icons/fc";
 const Register = () => {
   return (
-    <div className="flex justify-center items-center bg-white h-auto min-h-screen px-4 py-10">
+    <div className="flex flex-col justify-center items-center bg-[white]  h-auto min-h-screen px-4 ">
       <form className="border w-full max-w-[570px] p-6 sm:p-8 md:p-10 lg:p-12 rounded-md">
         <h1 className="text-[22px] sm:text-[24px] font-bold mb-10">
           Create an account
@@ -42,18 +44,38 @@ const Register = () => {
         </fieldset>
 
         <div className="w-full mt-8">
-          <button className="bg-[#F63E7B] text-white text-[16px] font-medium py-4 w-full rounded-lg">
+          <button className="bg-[#F63E7B] text-white text-[16px] font-medium py-4 w-full rounded-lg cursor-pointer">
             Create an account
           </button>
         </div>
 
         <p className="mt-6 text-center">
           Already have an account?{" "}
-          <Link to={"/login"} className="text-[#F63E7B] font-medium">
+          <Link
+            to={"/login"}
+            className="text-[#F63E7B] font-medium cursor-pointer"
+          >
             Login
           </Link>
         </p>
       </form>
+      <div className="flex items-center mt-5 gap-2">
+        <img className="h-[2px] w-[220px]" src={lineBar} alt="" />
+        <p>Or</p>
+        <img className="h-[2px] w-[220px]" src={lineBar} alt="" />
+      </div>
+      <div className="">
+        <button className="flex items-center w-[370px] border rounded-full gap-12 px-2 py-2 mt-4 cursor-pointer  font-medium text-[16px]">
+          <img src={facebook} alt="" />
+          <span>Continue with Facebook</span>
+        </button>
+      </div>
+      <div className="mt-4">
+        <button className="flex items-center border  rounded-full w-[370px] py-2 px-2 gap-12 font-medium text-[16px]">
+          <FcGoogle className="text-4xl" />
+          <span>Continue with Google</span>
+        </button>
+      </div>
     </div>
   );
 };
