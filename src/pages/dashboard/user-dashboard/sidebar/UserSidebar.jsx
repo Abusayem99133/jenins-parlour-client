4abbce688e14fd4efed032ee6159899c4af44cfc
Md.Rokonuzzaman Sayem <123319907+Abusayem99133@users.noreply.github.com>
2025-12-24 20:55:10 +0600

    Update UserSidebar.jsx

--- src/pages/dashboard/user-dashboard/sidebar/UserSidebar.jsx
+++ src/pages/dashboard/user-dashboard/sidebar/UserSidebar.jsx
@@ -86,7 +86,7 @@ const UserSidebar = () => {
         {navItems}
       </ul>
       {/* ---------- BOTTOM (User Info + Logout) ---------- */}
-      <div className="p-4 ">
+      <div className="p-5 ">
         {/* User profile */}
         <div className="flex items-center gap-3 mb-3">
           <img
