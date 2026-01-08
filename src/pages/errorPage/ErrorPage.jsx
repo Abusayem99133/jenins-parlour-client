27986ea83b48341f8a39dddbb6124ef9ea3d51bd
Md.Rokonuzzaman Sayem <123319907+Abusayem99133@users.noreply.github.com>
2026-01-08 20:53:22 +0600

    Update ErrorPage.jsx

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -4,7 +4,7 @@ const ErrorPage = () => {
   return (
     <div>
       <h1 className="text-red-700 btn btn-primary">error page</h1>
-      <h1>work on going</h1>
+      <h1>work on going to the code </h1>
     </div>
   );
 };
