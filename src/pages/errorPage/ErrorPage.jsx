03470b695b7a5b71fb563d16c50de1fd2ec16e08
Md.Rokonuzzaman Sayem <123319907+Abusayem99133@users.noreply.github.com>
2026-01-11 15:20:33 +0600

    Update ErrorPage.jsx

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ErrorPage = () => {
   return (
     <div>
-      <h1 className="text-red-700 btn btn-primary">error page</h1>
+      <h1 className="text-red-600 btn btn-primary">error page</h1>
       <h1>work on going</h1>
     </div>
   );
